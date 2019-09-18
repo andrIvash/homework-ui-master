7dcf705ef519d01d31bbbf62ca6eda12cf4270b9
Andriy Ivashchenko <andriy.ivashchenko@thomsonreuters.com>
2019-09-18 17:13:30 +0300

    Updates

---
+++ precache-manifest.657a3e105df0d6831e00d63900bb6e53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd49270bf58664822eeb50bf6410e44",
+    "url": "/homework-ui-master/index.html"
+  },
+  {
+    "revision": "27a017f86e43e51fdd46",
+    "url": "/homework-ui-master/static/css/main.fe5dc5dd.chunk.css"
+  },
+  {
+    "revision": "796855baa23935a86628",
+    "url": "/homework-ui-master/static/js/2.32176729.chunk.js"
+  },
+  {
+    "revision": "27a017f86e43e51fdd46",
+    "url": "/homework-ui-master/static/js/main.e2b83e8a.chunk.js"
+  },
+  {
+    "revision": "3b15059824ec366573e0",
+    "url": "/homework-ui-master/static/js/runtime~main.20252405.js"
+  }
+]);
